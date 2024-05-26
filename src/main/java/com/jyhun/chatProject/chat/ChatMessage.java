df99ab620ffd1b6ee2c5890c2fb04ace0d56addf
j0hun <dudgns3681@gmail.com>
2024-05-26 16:53:28 +0900

    STOMP와 SOCKJS 를 이용한 채팅방 구현

---
+++ src/main/java/com/jyhun/chatProject/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.jyhun.chatProject.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    private String sender;
+
+    private String content;
+
+}
